276b9e875678a1a5fb10bcbfb6ada2102fab6e24
linjonathan0220 <123289345+linjonathan0220@users.noreply.github.com>
2023-01-22 23:37:04 +1000

    feat: add new code for filter test case

--- cypress/e2e/filters.cy.ts
+++ cypress/e2e/filters.cy.ts
@@ -9,5 +9,40 @@ describe('Category Filters', () => {
 
     it('Race name should updated after deselect filer', () => {
         cy.visit('');
+
+        var Current_race = []
+        var New_race = []
+        //Store all the race name before uncheck some filter
+        cy.get('div').each(() => {
+            Current_race.push(cy.get('.race-name'))
+        })
+
+        //To uncheck the Thoroughbred filter
+        cy.get('[data-testid="category-filter-4a2788f8-e825-4d36-9894-efd4baf1cfae"] > [data-testid="category-filter-checkbox"]').uncheck();
+
+        //Store the race name after unchecked the Thoroughbred
+        cy.get('.item').each(() => {
+            New_race.push(cy.get('.race-name'))
+        })
+
+        //Compare the value in Old
+        cy.get(Current_race).should('not.equal', New_race)
+
+        //Copy New race to current race
+        Current_race == New_race
+
+        //To uncheck the Greyhound filter
+        cy.get('[data-testid="category-filter-9daef0d7-bf3c-4f50-921d-8e818c60fe61"] > [data-testid="category-filter-checkbox"]').uncheck();
+
+        //Store the race name after unchecked the Greyhound
+        cy.get('.item').each(() => {
+            New_race.push(cy.get('.race-name'))
+        })
+
+        //Compare the value in Old
+        cy.get(Current_race).should('not.equal', New_race)
+
+        //Copy New race to current race
+        Current_race == New_race
     })
 });

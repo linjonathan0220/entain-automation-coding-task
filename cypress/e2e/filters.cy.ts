718d2cdf79824430a39efc39571f088d171ab108
linjonathan0220 <123289345+linjonathan0220@users.noreply.github.com>
2023-01-22 23:33:27 +1000

    feat: Add code for unchecking all filters

--- cypress/e2e/filters.cy.ts
+++ cypress/e2e/filters.cy.ts
@@ -1,6 +1,10 @@
 describe('Category Filters', () => {
     it('Should have all filter enabled when all the checkboxs are unchecked', () => {
         cy.visit('');
+        //To uncheck all the checkbox in the application
+        cy.get('[type="checkbox"]').uncheck();
+        //Make sure the checkbox will be checked when last checkbox has been unchecked
+        cy.get('[type="checkbox"]').should('be.checked')
     })
 
     it('Race name should updated after deselect filer', () => {

a38e75d9718d461c7a51036de7bbce661df4c673
linjonathan0220 <123289345+linjonathan0220@users.noreply.github.com>
2023-01-22 23:41:56 +1000

    feat: Create 5 cases title for counterdown timer
    5 new test cases:
    1. Race should remove once it began
    2. Order of race should show in less time to most time left.
    3. Format of time should change once the time is less than 5 mins
    4. Format of time should change when the time is less than 1 min
    5. Reduce 1 sec each time when count down

--- cypress/e2e/countdown-timer.cy.ts
+++ cypress/e2e/countdown-timer.cy.ts
@@ -1,7 +1,20 @@
 describe('Countdown Timer', () => {
-  it.skip('Insert test here');
+    it('Should remove the race once the race is began', () => {
+        cy.visit('');
+    })
 
-  it.skip('Insert test here');
+    it('Should display the race from less time to most time', () => {
+        cy.visit('');
+    })
 
-  it.skip('Insert test here');
+    it('Should display in Minute and Second after 5 mins', () => {
+        cy.visit('');
+    })
+
+    it('Should display in second after 1 min', () => {
+        cy.visit('');
+    })
+
+    it('Should count down as expected by 1 sec each time', () => {
+        cy.visit('');
 });

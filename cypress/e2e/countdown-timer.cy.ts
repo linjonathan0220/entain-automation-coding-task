76b1344422f3001c3f63d1a19eeacfc51041df67
linjonathan0220 <123289345+linjonathan0220@users.noreply.github.com>
2023-01-22 23:45:49 +1000

    feat: Add code for checking the order of race list

--- cypress/e2e/countdown-timer.cy.ts
+++ cypress/e2e/countdown-timer.cy.ts
@@ -13,6 +13,19 @@ describe('Countdown Timer', () => {
 
     it('Should display the race from less time to most time', () => {
         cy.visit('');
+
+        let i = 0
+
+        // Create a Array to store all the race time left
+        var racetime1 = []
+
+        // Push current all rece time to the racetime1 array
+        for (i = 0; i < 5; i++) {
+            racetime1.push(cy.get('div.item').children('p').eq(i))
+        }
+
+        // Compare the race time array with race time array after sorted
+        cy.wrap(racetime1).should('equal', racetime1.sort())
     })
 
     it('Should display in Minute and Second after 5 mins', () => {
